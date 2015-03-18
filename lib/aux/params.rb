b884b335e2657460e03aedf56ff624aca1465180
Eric Schwarzenbach <easchwarzenbach@gmail.com>
2015-03-18 10:06:10 -0700

    reorganize file structure

---
+++ lib/aux/params.rb
@@ -0,0 +1,69 @@
+require 'uri'
+
+module Controller
+  class Params
+    # use your initialize to merge params from
+    # 1. query string
+    # 2. post body
+    # 3. route params
+    #
+    # You haven't done routing yet; but assume route params will be
+    # passed in as a hash to `Params.new` as below:
+    def initialize(req, route_params = {})
+      @params = route_params
+
+      unless req.query_string.nil?
+        query_params = parse_www_encoded_form(req.query_string)
+        @params.merge!(query_params)
+      end
+
+      unless req.body.nil?
+        body_params = parse_www_encoded_form(req.body)
+        @params.merge!(body_params)
+      end
+    end
+
+    def [](key)
+      @params[key.to_s]
+    end
+
+    def to_s
+      @params.to_json.to_s
+    end
+
+    class AttributeNotFoundError < ArgumentError; end;
+
+    private
+    # this should return deeply nested hash
+    # argument format
+    # user[address][street]=main&user[address][zip]=89436
+    # should return
+    # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
+    def parse_www_encoded_form(www_encoded_form)
+      query_params = {}
+      ary = URI::decode_www_form(www_encoded_form)
+
+      ary.each do |keyval|
+        val = keyval[1]
+        keys = parse_key(keyval[0])
+
+        cur_node = query_params
+        keys.each_with_index do |key, i|
+          if i == keys.length - 1
+            cur_node[key] = val
+            break
+          end
+          cur_node[key] ||= {}
+          cur_node = cur_node[key]
+        end
+      end
+      query_params
+    end
+
+    # this should return an array
+    # user[address][street] should return ['user', 'address', 'street']
+    def parse_key(key)
+      key.split(/\]\[|\[|\]/)
+    end
+  end
+end
---
+++ lib/base/controller_base.rb
@@ -0,0 +1,73 @@
+require 'active_support'
+require 'active_support/inflector'
+require 'active_support/core_ext'
+require 'erb'
+require_relative '../aux/params'
+require_relative '../aux/router'
+require_relative '../aux/session'
+
+module Controller
+  class Base
+    attr_reader :req, :res
+
+    # Setup the controller
+    def initialize(req, res, route_params = {})
+      @params = Params.new(req, route_params)
+      @req = req
+      @res = res
+    end
+
+    # Helper method to alias @already_built_response
+    def already_built_response?
+      !!@already_built_response
+    end
+
+    # Set the response status code and header
+    def redirect_to(url)
+      session.store_session(@res)
+
+      if already_built_response?
+        raise 'Multiple render actions detected'
+      end
+      @res.status = 302
+      @res['Location'] = url
+      @already_built_response = true
+    end
+
+    # Populate the response with content.
+    # Set the response's content type to the given type.
+    # Raise an error if the developer tries to double render.
+    def render_content(content, content_type)
+      session.store_session(@res)
+
+      if already_built_response?
+        raise 'Multiple render actions detected'
+      end
+      @already_built_response = true
+      @res.content_type = content_type
+      @res.body = content
+    end
+
+    # use ERB and binding to evaluate templates
+    # pass the rendered html to render_content
+    def render(template_name)
+      template_path = "views/#{self.class.name.underscore}/#{template_name.to_s}.html.erb"
+      erb_string = File.read(template_path)
+      erb_template = ERB.new(erb_string).result(binding)
+      render_content(erb_template, 'text/html')
+    end
+
+    # use this with the router to call action_name (:index, :show, :create...)
+    def invoke_action(name)
+      self.send(name)
+      unless @already_built_response
+        render(name)
+      end
+    end
+
+    # method exposing a `Session` object
+    def session
+      @session ||= Session.new(@req)
+    end
+  end
+end
